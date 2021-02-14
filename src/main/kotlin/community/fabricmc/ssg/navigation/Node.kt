0d56bfc1f1d8177e656079a232744bc38f326935
px01tv <78348979+px01tv@users.noreply.github.com>
2021-02-14 16:43:47 +0000

    Add spacer to Navigation node.
    Default false.

--- src/main/kotlin/community/fabricmc/ssg/navigation/Node.kt
+++ src/main/kotlin/community/fabricmc/ssg/navigation/Node.kt
@@ -7,10 +7,11 @@ public data class Node(
     val path: String,
     val title: String,
     val icon: String,
-
+    val spacer: Boolean = false,
+    
     val children: List<Node> = listOf(),
     val description: String? = null
 ) {
     override fun toString(): String =
-        "Node(path = \"$path\", title = \"$title\", icon = \"$icon\", children = [${children.joinToString(", ")}])"
+        "Node(path = \"$path\", title = \"$title\", icon = \"$icon\", children = [${children.joinToString(", ")}], spacer = \"$spacer\")"
 }
